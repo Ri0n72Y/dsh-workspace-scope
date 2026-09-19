21def148c35fc202f3df01967829c2438e7ee31c
Ri0n72Y <tzy673570751@gmail.com>
2026-09-19 19:17:47 +0800

    fix: model alpha.2 session slot props

--- src/client/model.ts
+++ src/client/model.ts
@@ -13,6 +13,8 @@ export interface ScopeDraft {
 }
 
 export interface DockProps {
+  sessionId?: string;
+  useSession?: (selector: (state: any) => any) => any;
   useSessions?: (selector: (state: any) => any) => any;
 }
 
